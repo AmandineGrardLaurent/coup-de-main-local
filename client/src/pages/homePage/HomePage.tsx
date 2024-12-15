36ebc33f24a7ede368ee52b844d3192457f6b77f
Amandine GRARD-LAURENT <a.grard.laurent08@gmail.com>
2024-12-15 15:58:36 +0100

    fix homepage

--- client/src/pages/homePage/HomePage.tsx
+++ client/src/pages/homePage/HomePage.tsx
@@ -1,12 +1,7 @@
 import style from "./homePage.module.css";
 import banner from "../../assets/images/entraide-banner.jpg";
-// import { useLoaderData } from "react-router-dom";
-// import { HelperType } from "../../lib/definitions";
-// import HelperCard from "../../components/helperCard/HelperCard";
 
 export default function HomePage() {
-  // const helper: HelperType[] = useLoaderData() as HelperType[];
-
   return (
     <div className={style.homePage}>
       <div>
@@ -18,9 +13,6 @@ export default function HomePage() {
           dans la ville de Bordeaux. Un quartier dynamique où la vie
           collaborative est mise en avant. Retrouvez la liste des services
           locaux et les annonces d'entraides entre particuliers.
-        </div>
-        <div>
-          <h2>Les échanges de services dans le quartier</h2>
         </div>
       </section>
     </div>
