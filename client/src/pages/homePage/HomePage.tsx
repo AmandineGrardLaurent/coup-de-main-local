e0e15e7d805a7ac31d1109760ba5657bb0daa7fe
Amandine GRARD-LAURENT <a.grard.laurent08@gmail.com>
2024-12-15 15:41:46 +0100

    fix homepage

--- client/src/pages/homePage/HomePage.tsx
+++ client/src/pages/homePage/HomePage.tsx
@@ -1,20 +1,7 @@
 import style from "./homePage.module.css";
 import banner from "../../assets/images/entraide-banner.jpg";
-import { HelperType, ServiceType } from "../../lib/definitions";
-import HelperCard from "../../components/helperCard/HelperCard";
-import { Link, useLoaderData } from "react-router-dom";
-import ServiceCard from "../../components/serviceCard/ServiceCard";
 
 export default function HomePage() {
-  const { helper, service } = useLoaderData() as {
-    helper: HelperType[];
-    service: ServiceType[];
-  };
-
-  if (!service || !helper) {
-    return <div>No data...</div>;
-  }
-
   return (
     <div className={style.homePage}>
       <div>
@@ -30,31 +17,11 @@ export default function HomePage() {
         <div>
           <h2>Les échanges de services dans le quartier</h2>
         </div>
-        <div className={style.cards}>
-          <Link to={`/helpers/${helper[1].id}`} className={style.link}>
-            <HelperCard helper={helper[1]} />
-          </Link>
-          {/* <Link to={`/helpers/${helper[5].id}`} className={style.link}>
-            <HelperCard helper={helper[5]} />
-          </Link>
-          <Link to={`/helpers/${helper[8].id}`} className={style.link}>
-            <HelperCard helper={helper[8]} />
-          </Link> */}
-        </div>
+        <div className={style.cards}></div>
         <div>
           <h2>Les services dans le quartier</h2>
         </div>
-        <div className={style.cards}>
-          <Link to={`/services/${service[1].id}`} className={style.link}>
-            <ServiceCard service={service[1]} />
-          </Link>
-          {/* <Link to={`/services/${service[5].id}`} className={style.link}>
-            <ServiceCard service={service[5]} />
-          </Link>
-          <Link to={`/services/${service[10].id}`} className={style.link}>
-            <ServiceCard service={service[10]} />
-          </Link> */}
-        </div>
+        <div className={style.cards}></div>
       </section>
     </div>
   );
