bd5142091268d64f143d46b6afd4b59f9d8e4630
Amandine GRARD-LAURENT <a.grard.laurent08@gmail.com>
2024-12-15 13:57:41 +0100

    fix : bug react-slick

--- client/src/pages/homePage/HomePage.tsx
+++ client/src/pages/homePage/HomePage.tsx
@@ -1,8 +1,8 @@
 import style from "./homePage.module.css";
-import Slider from "react-slick";
+// import Slider from "react-slick";
 import { Link, useLoaderData } from "react-router-dom";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
 import HelperCard from "../../components/helperCard/HelperCard";
 import banner from "../../assets/images/entraide-banner.jpg";
 import { HelperType, ServiceType } from "../../lib/definitions";
@@ -17,49 +17,49 @@ export default function HomePage() {
     return <div>No data...</div>;
   }
 
-  const isMobile = window.innerWidth < 720;
+  // const isMobile = window.innerWidth < 720;
 
-  const settings = {
-    dots: false,
-    infinite: true,
-    slidesToShow: isMobile ? 1 : 5,
-    slidesToScroll: 1,
-    autoplay: true,
-    speed: 2000,
-    autoplaySpeed: 1000,
-    centerMode: !isMobile,
-    centerPadding: "0",
-    cssEase: "ease-in-out",
-    vertical: false,
-    verticalSwiping: false,
-    responsive: [
-      {
-        breakpoint: 720,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          vertical: true,
-          verticalSwiping: true,
-        },
-      },
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 4,
-        },
-      },
-    ],
-  };
+  // const settings = {
+  //   dots: false,
+  //   infinite: true,
+  //   slidesToShow: isMobile ? 1 : 5,
+  //   slidesToScroll: 1,
+  //   autoplay: true,
+  //   speed: 2000,
+  //   autoplaySpeed: 1000,
+  //   centerMode: !isMobile,
+  //   centerPadding: "0",
+  //   cssEase: "ease-in-out",
+  //   vertical: false,
+  //   verticalSwiping: false,
+  //   responsive: [
+  //     {
+  //       breakpoint: 720,
+  //       settings: {
+  //         slidesToShow: 1,
+  //         slidesToScroll: 1,
+  //         vertical: true,
+  //         verticalSwiping: true,
+  //       },
+  //     },
+  //     {
+  //       breakpoint: 1024,
+  //       settings: {
+  //         slidesToShow: 4,
+  //       },
+  //     },
+  //   ],
+  // };
 
-  const renderHelpers = () => {
-    return helpers.map((helper) => (
-      <div className={style.cardContainer2} key={helper.id}>
-        <Link to={`/helpers/${helper.id}`} className={style.link}>
-          <HelperCard helper={helper} />
-        </Link>
-      </div>
-    ));
-  };
+  // const renderHelpers = () => {
+  //   return helpers.map((helper) => (
+  //     <div className={style.cardContainer2} key={helper.id}>
+  //       <Link to={`/helpers/${helper.id}`} className={style.link}>
+  //         <HelperCard helper={helper} />
+  //       </Link>
+  //     </div>
+  //   ));
+  // };
 
   return (
     <div className={style.homePage}>
@@ -68,9 +68,9 @@ export default function HomePage() {
       </div>
 
       <h2 className={style.homePageHelpersTitle}>Annonces d'entraide</h2>
-      <Slider className="sliderStyle" {...settings}>
+      {/* <Slider className="sliderStyle" {...settings}>
         {renderHelpers()}
-      </Slider>
+      </Slider> */}
     </div>
   );
 }
